9860cda8f608ad97891ba38b32477d751d0bc267
Nikhil Rawal <rawal.nikhil29@gmail.com>
2023-07-07 18:17:16 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+    const element = React.createElement("h1",{id:"heading"},"Hello, this is from ReactJS")
+
+    root.render(element)
