39993e3609dae4e38678f190bf142ea088e45f64
Nikhil Rawal <rawal.nikhil29@gmail.com>
2023-11-30 22:40:54 -0500

    node-modules problem - needs change urgently

--- src/components/DishesCollectionCard.js
+++ src/components/DishesCollectionCard.js
@@ -1,15 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import ConsiderDishesCard from "./ConsiderDishesCard";
 import { RESTAURANT_API } from "../utils/constants";
 
 const DishesCollectionCard = () => {
   const { dishName, dishID } = useParams();
-  console.log(dishID, dishName);
-  console.log(
-    RESTAURANT_API +
-      "80423&tags=layout_CCS_CholeBhature&sortBy=&filters=&type=rcv2"
-  );
+
+  // useEffect(() => {
+  //   fetchAllDishesRestaurantsData();
+  // }, []);
+
+  const fetchAllDishesRestaurantsData = async () => {
+    const data = await fetch(
+      `${RESTAURANT_API}&collection=${dishID}&tags=layout_CCS&sortBy=&filters=&type=rcv2`
+    );
+    const json = await data.json();
+    const jsonAPIData = json;
+    console.log(jsonAPIData);
+  };
+  // console.log(dishID, dishName);
+  // console.log(
+  //   "abc",
+  //   `${RESTAURANT_API}&collection=${dishID}&tags=layout_CCS&sortBy=&filters=&type=rcv2`
+  // );
   return (
     <div>
       <h1>Our favourite restaurants curated for {dishName}</h1>
