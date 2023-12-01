8971281700893102807178e031d72d06e29eddeb
Nikhil Rawal <rawal.nikhil29@gmail.com>
2023-11-30 22:10:54 -0500

    final app for dishes - onclick

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React, { lazy, useContext } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Body from "./components/Body";
@@ -11,6 +11,7 @@ import RestaurantCard from "./components/RestaurantCard";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Cart from "./components/Cart";
+import DishesCollectionCard from "./components/DishesCollectionCard";
 
 // const Grocery = lazy(()=>import("./components/Grocery"))
 // const About = lazy(() => import("./components/About"));
@@ -51,6 +52,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/cart",
         element: <Cart />,
+      },
+      {
+        path: "/dishesCollection/:dishName/:dishID",
+        element: <DishesCollectionCard />,
       },
     ],
     errorElement: <Error />,
