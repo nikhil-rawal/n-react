892ba309732cb448e09b6bee0126f26486f28182
Nikhil Rawal <rawal.nikhil29@gmail.com>
2023-11-30 22:07:47 -0500

    edited const for images - dishes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,4 +12,8 @@ export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6809111&lng=77.2045867&restaurantId=";
 
 export const DISHES_CDN_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029844/PC_Creative%20refresh/3D_bau/banners_new/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
+
+export const DISHES_INDIVIDUAL_URL = `${RESTAURANT_API}&collection=`;
+
+// https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6809111&lng=77.2045867&collection=80423&tags=layout_CCS_CholeBhature&sortBy=&filters=&type=rcv2
